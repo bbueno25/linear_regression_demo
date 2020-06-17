e0138974a387530a4f7b5bd5651ba47f00ff5a07
B. Bueno <bbueno25@hotmail.com>
2020-06-17 00:15:46 -0600

    How to Make a Prediction - Intro to Deep Learning #1

--- linear_regression_demo/linear_regression_demo.py
+++ linear_regression_demo/linear_regression_demo.py
@@ -1,1 +1,15 @@
+"""
+DOCSTRING
+"""
+import matplotlib.pyplot as pyplot
+import pandas
+import sklearn.linear_model as linear_model
 
+dataframe = pandas.read_fwf('brain_body.txt')
+x_values = dataframe[['Brain']]
+y_values = dataframe[['Body']]
+body_reg = linear_model.LinearRegression()
+body_reg.fit(x_values, y_values)
+pyplot.scatter(x_values, y_values)
+pyplot.plot(x_values, body_reg.predict(x_values))
+pyplot.show()
